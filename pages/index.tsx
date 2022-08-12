0be112536b72087626ef3358ea8490c7eaab521c
seungo <ilkhso@gmail.com>
2022-08-12 15:09:06 +0900

    add: google login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next';
 import { Box, Center, Flex, Heading } from '@chakra-ui/react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { ServiceLayout } from '@/components/service-layout';
 import { GoogleLoginButton } from '@/components/google-login-button';
+import FirebaseClient from '@/models/firebase-client';
+
+const provider = new GoogleAuthProvider();
 
 const IndexPage: NextPage = function () {
   return (
@@ -13,7 +17,15 @@ const IndexPage: NextPage = function () {
         </Box>
       </Flex>
       <Center mt="20">
-        <GoogleLoginButton />
+        <GoogleLoginButton
+          onClick={() => {
+            signInWithPopup(FirebaseClient.getInstance().Auth, provider)
+              .then((result) => {
+                console.info(result.user);
+              })
+              .catch((error) => console.log(error));
+          }}
+        />
       </Center>
     </ServiceLayout>
   );
